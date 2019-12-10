a0ca87bb3417dd9ba6d16c6c7b83baf78c313025
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-10 02:22:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
+    binding.pry
     @user = User.new(username: params["username"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
-
     redirect '/tweets'
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191209215613) do
-
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", limit: 6, null: false
-    t.datetime "updated_at", limit: 6, null: false
-  end
-
-  add_index "ar_internal_metadata", ["key"], name: "sqlite_autoindex_ar_internal_metadata_1", unique: true
-
-  create_table "tweets", force: :cascade do |t|
-    t.string  "content"
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
