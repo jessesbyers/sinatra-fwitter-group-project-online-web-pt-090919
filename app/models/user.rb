5d77c07edc90d48c2cbb848e395c8d112b230ff4
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-11 13:08:49 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
 
-  # def slug
-  #   User.find(id).username.gsub(" ", "-")
-  # end
-  #
-  # def self.find_by_slug(slug)
-  #   User.find_by(slug)
-  # end
+  def slug
+    User.find(id).username.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.find_by(slug)
+  end
 end
